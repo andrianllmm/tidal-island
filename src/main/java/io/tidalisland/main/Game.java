3d7dff446597ec61f9d909ddc9b098c6f5a5054c
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-11-11 14:17:06 +0800

    chore: fix style warnings

--- src/main/java/io/tidalisland/main/Game.java
+++ src/main/java/io/tidalisland/main/Game.java
@@ -1,7 +1,10 @@
 package io.tidalisland.main;
 
+/**
+ * Main entry point.
+ */
 public class Game {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(final String[] args) {
+    System.out.println("Hello World!");
+  }
 }
