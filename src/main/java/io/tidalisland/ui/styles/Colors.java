75de56e5926044c351744ca6b6ff7798398229cd
Andrian Lloyd Maagma <110181829+andrianllmm@users.noreply.github.com>
2025-12-19 01:32:47 +0800

    feat: redesign ui colors

---
+++ src/main/java/io/tidalisland/ui/styles/Colors.java
@@ -0,0 +1,21 @@
+package io.tidalisland.ui.styles;
+
+import java.awt.Color;
+
+/**
+ * Colors used in the UI.
+ */
+public class Colors {
+  public static final Color WHITE = new Color(225, 216, 203);
+  public static final Color GREEN = new Color(73, 88, 64);
+  public static final Color RED = new Color(131, 49, 33);
+  public static final Color BEIGE = new Color(166, 138, 100);
+
+  public static final Color PRIMARY = new Color(81, 72, 64);
+  public static final Color PRIMARY_LIGHT = new Color(101, 92, 84);
+  public static final Color PRIMARY_DARK = new Color(61, 56, 50);
+  public static final Color PANEL = new Color(69, 64, 56);
+  public static final Color TEXT = WHITE;
+  public static final Color BORDER = new Color(55, 45, 35);
+  public static final Color BORDER_LIGHT = new Color(75, 65, 55);
+}
